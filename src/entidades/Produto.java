b2695e0a81e3d0e192dc6f70315b8c860370ad04
Willian Thomaz <willian_thomaz@outlook.com>
2020-12-22 14:08:49 -0300

    Exercicio de Polimorfismo

---
+++ src/entidades/Produto.java
@@ -0,0 +1,38 @@
+package entidades;
+
+public class Produto {
+	
+	private String nome;
+	private Double preco;
+	
+	public Produto(String nome, Double preco) {
+		this.nome = nome;
+		this.preco = preco;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+	
+	public String etiquetaPreco() {
+		
+		return  nome + " $ " + String.format("%.2f", preco);
+	}
+	
+	public String toString() {
+		return etiquetaPreco();
+	}
+
+}
